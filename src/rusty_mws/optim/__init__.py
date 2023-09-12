9dc5dd8253e8672d857a18aa83c2d27037011c20
brianreicher <reicher.b@northeastern.edu>
2023-09-12 10:49:22 -0400

    Passing tests, update array mem rewrite

--- src/rusty_mws/optim/__init__.py
+++ src/rusty_mws/optim/__init__.py
@@ -1,2 +1,2 @@
 from .genetic_optim import GeneticOptimizer
-from .gridsearch_optim import grid_search
+from .gridsearch_optim import GridSearchOptimizer
--- tests/filter_test.py
+++ tests/filter_test.py
@@ -5,21 +5,21 @@ import numpy as np
 import pytest
 
 
-# stage data for testing
-affs_data = np.array([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])
-fragments_data = np.array([1, 2, 1, 3])
-filter_val = 0.5
-
-
 def test_filter_partial():
     # Test when all fragments should be filtered out
+    affs_data = np.array([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])
+    fragments_data = np.array([1, 2, 1, 3])
+    filter_val = 0.5
     rusty_mws.utils.filter_fragments(affs_data, fragments_data, filter_val)
     assert np.array_equal(fragments_data, np.array([0, 0, 0, 3]))
 
 
 def test_filter_above_threshold():
     # Test when no fragments should be filtered out
-    rusty_mws.utils.filter_fragments(affs_data, fragments_data, 0.0)
+    affs_data = np.array([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])
+    fragments_data = np.array([1, 2, 1, 3])
+    filter_val = 0.0
+    rusty_mws.utils.filter_fragments(affs_data, fragments_data, filter_val)
     assert np.array_equal(fragments_data, np.array([1, 2, 1, 3]))
 
 
